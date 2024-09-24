fc68407d3564c74a0d70ea1aab0180dbd7756637
leorvergani <vergani@outlook.com.br>
2024-09-23 21:23:56 -0300

    Biscoito da sorte

--- App.tsx
+++ App.tsx
@@ -1,12 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+import LottieView  from 'lottie-react-native'
+import { useState } from 'react';
+
+const mensagem = [
+  'mensagem 1',
+  'mensagem 2',
+  'mensagem 3',
+  'mensagem 4',
+  'mensagem 5'
+]
+
+
 
 export default function App() {
+
+  const [mensagemSelecionada, setMensagemSelecionada] = useState(' ')
+  
+
+  function exibitMensagem() {
+    const numeroAleatorio = Math.floor(Math.random() * 5)
+    setMensagemSelecionada(mensagem[numeroAleatorio])
+    //mensagemSelecionado = mensagens[numeroAleatorio]
+    // Alert.alert(mensagemSelecionado)
+}
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+
+      <Text>{mensagemSelecionada}</Text>
+
+      <LottieView 
+        autoPlay
+        source={require('./assets/cookie.json')}
+        style={{
+          width: 200,
+          height: 200
+        }}
+      />
+
+      <Button onPress={exibitMensagem} title='Quebrar biscoito'/>
+
+
+    </SafeAreaView>
   );
 }
 
